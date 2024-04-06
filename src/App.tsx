3018b29b40796203983f24a8a5d464f6a807dbf9
nicholasboari <nicholasboari@gmail.com>
2024-04-06 20:40:25 -0300

    feat: about component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './components/about/About';
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
---
+++ src/components/about/About.tsx
@@ -0,0 +1,15 @@
+import "./about.css"
+
+const About = () => {
+    return <>
+        <div className="container">
+            <img src="src/assets/img/me2.jpg" alt="foto do nicholas" className="photo" />
+            <div className="about">
+                <h2>Olá,</h2>
+                <span>Sou um desenvolvedor de software backend com experiência em Java e outras tecnologias. Me considero uma pessoa de rápido aprendizado e estou sempre aberto a assumir novas tarefas e desafios.</span>
+            </div>
+        </div>
+    </>
+}
+
+export default About
